f59aef03764df1fd9b53e64d557531a19be835a8
James Boehme <jamesrbdev@gmail.com>
2021-03-27 00:34:08 -0500

    Further css changes, set links to open in new tab. Minor commenting.

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+/*
+	Hello! Interested in offering me a position at your company?
+	Drop me an email at: jamesrbdev@gmail.com 
+*/
+
 console.log("%cHello! Let me know how I can improve by dropping me an email at: %cjamesrbdev@gmail.com", "font-size: 18px;", "font-size: 18px; color: lightblue; text-decoration: underline;")
 
+// Navigation button.
 function navigate(page) { // (number)
-	let pages = document.getElementById("pages");
+	let pages = document.getElementById("pages"); // Get the pages element.
 
-	for (let i = 0; i < 4; ++i) {
-		if (i === page) {
-			document.getElementById("nav-" + i).classList.add("active");
-		} else {
-			document.getElementById("nav-" + i).classList.remove("active");
+	for (let i = 0; i < 4; ++i) { // Loop through all nav buttons.
+		if (i === page) { // If it is the button that was just clicked:
+			document.getElementById("nav-" + i).classList.add("active"); // Activate nav button.
+		} else { // Otherwise:
+			document.getElementById("nav-" + i).classList.remove("active"); // Deactivate other nav buttons.
 		}
 	}
 
-
-
-	pages.style.top = -(page * 100) + "%";
+	pages.style.top = -(page * 100) + "%"; // Changes the page to the specified number.
 }
