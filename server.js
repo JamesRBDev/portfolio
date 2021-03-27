c10a134a8756c0e92b1f7291e7ca249a19d18a36
James Boehme <jamesrbdev@gmail.com>
2021-03-27 11:00:38 -0500

    Major server functionality expansion.

--- server.js
+++ server.js
@@ -19,8 +19,54 @@
 const express = require("express");
 const app     = express();
 
+// Log errors.
+process.on('uncaughtException', function (err) {
+	console.log('Uncaught exception: ' + err.stack); // Log the error.
+	shutdown(); // Shut down the server.
+});
+
 // Use the Express-static middleware.
 app.use(express.static("public"));
 
 // Start the server listening for requests.
-app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+let server = app.listen(process.env.PORT || 3000, () => console.log("SERVER OPEN ON PORT " + process.env.PORT || 3000));
+
+// Shuts down the server.
+function shutdown() {
+	if (!server || server.closing) {
+		console.log("Shutdown forced.")
+		process.exit(42);
+	} else {
+		console.log('Shutting down...');
+		// Stop server.
+		server.stop(() => {
+			// Close process with success.
+			console.log("Server shut down.");
+			process.exit(0);
+		});
+
+		// Handle timeouts.
+		setTimeout(() => {
+			console.log("Shutdown timed out.")
+			process.exit(0);
+		}, 1000);
+	}
+}
+
+// Console input handling.
+readline.on('line', (input) => {
+	switch (input) {
+		case "shutdown": case "exit": case "close": case "stop": // Shuts down the server.
+			shutdown();
+			break;
+		case "forcekill": case "kill": case "forcestop": case "forceclose": // Force kills the server.
+			process.exit(0);
+			break;
+		default: // Unrecognized command.
+			console.log("Unrecognized command.");
+	}
+});
+
+// Shutdown signals.
+process.on("SIGTERM", shutdown);
+process.on("SIGINT",  shutdown);
