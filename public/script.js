14f16d3e2405f00bf8e74c620dc280876fe54fa9
James Boehme <jamesrbdev@gmail.com>
2021-11-21 22:19:40 -0600

    Major changes to pretty much everything.

--- public/script.js
+++ public/script.js
@@ -16,19 +16,32 @@
 // FUNCTIONS                                                        //
 //——————————————————————————————————————————————————————————————————//
 
+var oldPage = "about";
+
 console.log("%cHello! Let me know how I can improve by dropping me an email at: %cjamesrbdev@gmail.com", "font-size: 18px;", "font-size: 18px; color: lightblue; text-decoration: underline;")
 
-// Navigation button.
-function navigate(page) { // (number)
-	let pages = document.getElementById("pages"); // Get the pages element.
+function start() {
+	let conElem   = document.getElementById("container");
+	let mainElem  = document.getElementById("window-main")
+	conElem.style = "height: 700px; width: 900px;"
 
-	for (let i = 0; i < 4; ++i) { // Loop through all nav buttons.
-		if (i === page) { // If it is the button that was just clicked:
-			document.getElementById("nav-" + i).classList.add("active"); // Activate nav button.
-		} else { // Otherwise:
-			document.getElementById("nav-" + i).classList.remove("active"); // Deactivate other nav buttons.
-		}
-	}
+	document.getElementById("window-entry").hidden = true;
 
-	pages.style.top = -(page * 100) + "%"; // Changes the page to the specified number.
+	mainElem.hidden = undefined;
+	setTimeout(() => mainElem.style.opacity = 1, 100);
 }
+
+function setPage(newPage) {
+	document.getElementById(`navlink-${oldPage}`).classList.remove("active");
+	document.getElementById(`navlink-${newPage}`).classList.add("active");
+
+	document.getElementById(`navlink-${oldPage}`).classList.remove("active");
+	document.getElementById(`navlink-${newPage}`).classList.add("active");
+
+	document.getElementById(`page-${oldPage}`).hidden = true;
+	document.getElementById(`page-${newPage}`).hidden = undefined;
+
+	oldPage = newPage;
+}
+
+setTimeout(() => document.getElementById("background").style.filter = "blur(0px)", 500);
