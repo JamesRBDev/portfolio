f6752cc3fb3ad25676ac6dea21417d39581da621
James Boehme <jamesrbdev@gmail.com>
2021-11-25 10:18:17 -0600

    Fix code format, alignment, and organization

--- public/script.js
+++ public/script.js
@@ -1,26 +1,30 @@
-/*
-	Hello! Interested in offering me a position at your company?
-	Drop me an email at: jamesrbdev@gmail.com 
-*/
+//———————————————————————————————————————————————————————————————————————//
+// SECTION Information
+/*———————————————————————————————————————————————————————————————————————//
 
-//——————————————————————————————————————————————————————————————————//
-// INFORMATION                                                      //
-//——————————————————————————————————————————————————————————————————//
+Author:      James Boehme / JamesRBDev
+Email:       jamesrbdev@gmail.com
+Description: The main site functionality. Mostly navigational in nature.
 
-// Contributors: James Boehme
+// !SECTION —————————————————————————————————————————————————————————————*/
 
-// This script is almost entirely dedicated to the navigational
-// buttons' functionality. 
+//———————————————————————————————————————————————————————————————————————//
+// SECTION Variables
+//———————————————————————————————————————————————————————————————————————//
 
-//——————————————————————————————————————————————————————————————————//
-// FUNCTIONS                                                        //
-//——————————————————————————————————————————————————————————————————//
-
+// The current page. Used for cleanup when the page changes.
 var oldPage = "about";
+
+// !SECTION —————————————————————————————————————————————————————————————//
+
+//———————————————————————————————————————————————————————————————————————//
+// SECTION Functions
+//———————————————————————————————————————————————————————————————————————//
 
 console.log("%cHello! Let me know how I can improve by dropping me an email at: %cjamesrbdev@gmail.com", "font-size: 18px;", "font-size: 18px; color: orange; text-decoration: underline;")
 
-function start() {
+// Transitions from the splash screen to the actual portfolio.
+function start() { // ()
 	let conElem   = document.getElementById("container");
 	let mainElem  = document.getElementById("window-main")
 	conElem.style = "height: 720px; width: 900px;"
@@ -28,10 +32,12 @@ function start() {
 	document.getElementById("window-entry").hidden = true;
 
 	mainElem.hidden = undefined;
+
 	setTimeout(() => mainElem.style.opacity = 1, 100);
 }
 
-function setPage(newPage) {
+// Sets the visible portfolio section.
+function setPage(newPage) { // (string)
 	document.getElementById(`navlink-${oldPage}`).classList.remove("active");
 	document.getElementById(`navlink-${newPage}`).classList.add("active");
 
@@ -44,4 +50,7 @@ function setPage(newPage) {
 	oldPage = newPage;
 }
 
+// Fades out initial background blur.
 setTimeout(() => document.getElementById("background").style.filter = "blur(0px)", 500);
+
+// !SECTION —————————————————————————————————————————————————————————————//
